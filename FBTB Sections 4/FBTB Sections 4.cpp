d9e341076d7fa942853a114b66c5b8bdeb12e8bc
Jesher Appels <jappels@zenztech.com>
2022-12-12 14:25:39 +0100

    finished section 4

--- FBTB Sections 4/FBTB Sections 4.cpp
+++ FBTB Sections 4/FBTB Sections 4.cpp
@@ -2,12 +2,13 @@
 
 int main()
 {
-	std::cout << "Enter your favorite number between 1 and 100: ";
+	std::cout << "Enter your favourite number between 1 and 100: ";
 
 	int favnr = { 0 };
 	std::cin >> favnr;
 
-	std::cout << "Amazing!! That's my favorie number too!" << std::endl;
+	std::cout << "Amazing!! That's my favourite number too! \n";
+	std::cout << "No really, " << favnr << " is my favourite number! \n";
 
 	return 0;
 }
