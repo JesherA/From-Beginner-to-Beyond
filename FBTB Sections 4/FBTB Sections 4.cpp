17871b53e383e7c9a2cbc623cfbc929fdadcd88d
Jesher Appels <jappels@zenztech.com>
2022-12-12 11:56:40 +0100

    initial commit

---
+++ FBTB Sections 4/FBTB Sections 4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Enter your favorite number between 1 and 100: ";
+
+	int favnr = { 0 };
+	std::cin >> favnr;
+
+	std::cout << "Amazing!! That's my favorie number too!" << std::endl;
+
+	return 0;
+}
